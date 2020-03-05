a8b4a2a49bd4c91a939035dad73359e3465f05df
Alword <anzer987@yandex.ru>
2020-03-05 18:09:07 +0300

    style entire api functions and update database

--- src/script.sql
+++ src/script.sql
@@ -1,100 +1,100 @@
 CREATE TABLE exam_type
 (
-id INT NOT NULL,
-type TEXT NULL
+    id                   INT AUTO_INCREMENT NOT NULL,
+    type                 TEXT NULL
 );
 
 ALTER TABLE exam_type
-ADD PRIMARY KEY (id);
+    ADD PRIMARY KEY (id);
 
 CREATE TABLE journal
 (
-id INT NOT NULL,
-study_plan_id INT NOT NULL,
-mark_id INT NOT NULL,
-journal_id INT NOT NULL,
-count INT NULL,
-in_time BIT NULL
+    id                   INT AUTO_INCREMENT NOT NULL,
+    study_plan_id        INT NOT NULL,
+    mark_id              INT NOT NULL,
+    student_id           INT NOT NULL,
+    count                INT NULL,
+    in_time              BIT NULL
 );
 
 ALTER TABLE journal
-ADD PRIMARY KEY (id);
+    ADD PRIMARY KEY (id);
 
 CREATE TABLE mark
 (
-id INT NOT NULL,
-name TEXT NULL,
-value TEXT NULL
+    id                   INT AUTO_INCREMENT NOT NULL,
+    name                 TEXT NULL,
+    value                TEXT NULL
 );
 
 ALTER TABLE mark
-ADD PRIMARY KEY (id);
+    ADD PRIMARY KEY (id);
 
 CREATE TABLE student
 (
-id INT NOT NULL AUTO_INCREMENT,
-study_group_id INT NOT NULL,
-surname TEXT NULL,
-name TEXT NULL,
-second_name TEXT NULL
+    id                   INT AUTO_INCREMENT NOT NULL,
+    study_group_id       INT NOT NULL,
+    surname              TEXT NULL,
+    name                 TEXT NULL,
+    second_name          TEXT NULL
 );
 
 ALTER TABLE student
-ADD PRIMARY KEY (id);
+    ADD PRIMARY KEY (id);
 
 CREATE TABLE study_group
 (
-id INT NOT NULL,
-name TEXT NULL
+    id                   INT AUTO_INCREMENT NOT NULL,
+    name                 TEXT NULL
 );
 
 ALTER TABLE study_group
-ADD PRIMARY KEY (id);
+    ADD PRIMARY KEY (id);
 
 CREATE TABLE study_plan
 (
-id INT NOT NULL,
-exam_type_id INT NOT NULL,
-subject_id INT NOT NULL
+    id                   INT AUTO_INCREMENT NOT NULL,
+    exam_type_id         INT NOT NULL,
+    subject_id           INT NOT NULL
 );
 
 ALTER TABLE study_plan
-ADD PRIMARY KEY (id);
+    ADD PRIMARY KEY (id);
 
 CREATE TABLE subject
 (
-id INT NOT NULL,
-name TEXT NULL,
-short_name TEXT NULL
+    id                   INT AUTO_INCREMENT NOT NULL,
+    name                 TEXT NULL,
+    short_name           TEXT NULL
 );
 
 ALTER TABLE subject
-ADD PRIMARY KEY (id);
+    ADD PRIMARY KEY (id);
 
 ALTER TABLE journal
-ADD CONSTRAINT R_1 FOREIGN KEY (STUDENT_ID) REFERENCES student (id);
+    ADD CONSTRAINT R_1 FOREIGN KEY (student_id) REFERENCES student (id);
 
 ALTER TABLE journal
-ADD CONSTRAINT R_2 FOREIGN KEY (mark_id) REFERENCES mark (id);
+    ADD CONSTRAINT R_2 FOREIGN KEY (mark_id) REFERENCES mark (id);
 
 ALTER TABLE journal
-ADD CONSTRAINT R_3 FOREIGN KEY (study_plan_id) REFERENCES study_plan (id);
+    ADD CONSTRAINT R_3 FOREIGN KEY (study_plan_id) REFERENCES study_plan (id);
 
 ALTER TABLE student
-ADD CONSTRAINT R_4 FOREIGN KEY (study_group_id) REFERENCES study_group (id);
+    ADD CONSTRAINT R_4 FOREIGN KEY (study_group_id) REFERENCES study_group (id);
 
 ALTER TABLE study_plan
-ADD CONSTRAINT R_5 FOREIGN KEY (subject_id) REFERENCES subject (id);
+    ADD CONSTRAINT R_5 FOREIGN KEY (subject_id) REFERENCES subject (id);
 
 ALTER TABLE study_plan
-ADD CONSTRAINT R_6 FOREIGN KEY (exam_type_id) REFERENCES exam_type (id);
+    ADD CONSTRAINT R_6 FOREIGN KEY (exam_type_id) REFERENCES exam_type (id);
 
 INSERT INTO subject (id, name, short_name) VALUES
 (1, 'Проектирование информационных систем', 'ПрИС'),
 (2, 'Системы искусственного интеллекта', 'СИИ'),
 (3, 'Программная инженерия', 'ПИ'),
 (4, 'Национальная система информационной безопасности', 'НСИБ'),
-(5, 'Системный анализ', 'СА'),
+(5, 'Системный анализ', 'СисАнал'),
 (6, 'Распределенные базы данных', 'РБД'),
 (7, 'Системное программное обеспечение', 'СПО');
 
